1b71a7233fd491f33ef4910ad06acfc7a87dbbb0
erni-services.jose.ramon.counago.roza <erni-services.jose.ramon.counago.roza@hp.com>
2017-04-15 19:28:25 +0200

    gulp npm workflows

---
+++ codigos/gulp-vs-npm/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', () => {
+	return gulp
+		.src('./style.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./'))
+});
+
+gulp.task('default', ['sass'], () => {
+	gulp.watch('./style.scss', ['sass'])
+})
+
+function es() {
+
+}
